4795e3e7735cbbf6d3e5293d389a033b0f174128
Kedar7833 <Kedar7833@users.noreply.github.com>
2019-04-06 19:14:21 -0400

    Changes to log and activity log class and xml respectively.

--- app/src/main/java/com/example/autismawareness/Login.java
+++ app/src/main/java/com/example/autismawareness/Login.java
@@ -3,40 +3,27 @@ package com.example.autismawareness;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import java.util.HashMap;
+
 
 public class Login extends AppCompatActivity {
-    EditText edt_title, edt_content;
-    Button btn_post;
-    RecyclerView recyclerView;
-    FirebaseDatabase database;
-    DatabaseReference reference;
-    @Override
+    private Button login;
+    private HashMap<String, String> login_information;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        edt_content = (EditText) findViewById(R.id.edit_content);
-        edt_title = (EditText) findViewById(R.id.edit_title);
-        btn_post = (Button) findViewById(R.id.btn_post);
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        database = FirebaseDatabase.getInstance();
-        reference = database.getReference("EDMT_FIREBASE");
-
-        btn_post.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               postComment();
-           }
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
         });
-
-    }
-    private void postComment() {
-        String title = edt_title.getText().toString();
-        String content = edt_content.getText().toString();
+        }
     }
 }

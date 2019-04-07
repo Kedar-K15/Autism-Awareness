f76e201642435029b6ac9ce360505082be593803
Kedar7833 <Kedar7833@users.noreply.github.com>
2019-04-07 04:52:21 -0400

    Add functionality to save Edit texts' for all options.

--- app/src/main/java/com/example/autismawareness/DailyRoutine.java
+++ app/src/main/java/com/example/autismawareness/DailyRoutine.java
@@ -1,13 +1,70 @@
 package com.example.autismawareness;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DailyRoutine extends AppCompatActivity {
+    private Button home;
+    private Button save;
+    private EditText wake_time;
+    private EditText breakfast;
+    private EditText lunch;
+    private EditText dinner;
+    private EditText sleep;
+    private static String str_wake;
+    private static String str_breakfast;
+    private static String str_lunch;
+    private static String str_dinner;
+    private static String sleep_time;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_routine);
+
+        wake_time = (EditText) findViewById(R.id.waketime_Data);
+        breakfast = (EditText) findViewById(R.id.breakfeat_data);
+        lunch = (EditText) findViewById(R.id.lunch_data);
+        dinner = (EditText) findViewById(R.id.Dinner_data);
+        sleep = (EditText) findViewById(R.id.Sleeptime_data);
+        home = (Button) findViewById(R.id.aaware_home);
+        save = (Button) findViewById(R.id.save_routine);
+        wake_time.setText(str_wake);
+        breakfast.setText(str_breakfast);
+        lunch.setText(str_lunch);
+        dinner.setText(str_dinner);
+        sleep.setText(sleep_time);
+
+
+
+
+        home = (Button) findViewById(R.id.aaware_home);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DailyRoutine.this, CategoryList.class));
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                str_wake = wake_time.getText().toString();
+                str_breakfast = dinner.getText().toString();
+                str_lunch = lunch.getText().toString();
+                str_dinner =  dinner.getText().toString();
+                sleep_time = sleep.getText().toString();
+
+                startActivity(new Intent(DailyRoutine.this, CategoryList.class));
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/autismawareness/HealthPanel.java
+++ app/src/main/java/com/example/autismawareness/HealthPanel.java
@@ -5,13 +5,30 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class HealthPanel extends AppCompatActivity {
     private Button home;
+    private Button save;
+    private  EditText blood_type;
+    private  EditText weight;
+    private  EditText height;
+    private static String str_blood_type;
+    private static String str_weight;
+    private static String str_height;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_panel);
+
+        blood_type = (EditText) findViewById(R.id.blood_type_data);
+        weight = (EditText) findViewById(R.id.weight_data);
+        height = (EditText) findViewById(R.id.height_data);
+        home = (Button) findViewById(R.id.aaware_home);
+        save = (Button) findViewById(R.id.save_emergency);
+        blood_type.setText(str_blood_type);
+        weight.setText(str_weight);
+        height.setText(str_height);
 
         home = (Button) findViewById(R.id.aaware_home);
 
@@ -21,5 +38,25 @@ public class HealthPanel extends AppCompatActivity {
                 startActivity(new Intent(HealthPanel.this, CategoryList.class));
             }
         });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HealthPanel.this, CategoryList.class));
+            }
+        });
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                str_blood_type = blood_type.getText().toString();
+                str_weight = weight.getText().toString();
+                str_height = height.getText().toString();
+                startActivity(new Intent(HealthPanel.this, CategoryList.class));
+            }
+        });
     }
 }
+
+
+
+

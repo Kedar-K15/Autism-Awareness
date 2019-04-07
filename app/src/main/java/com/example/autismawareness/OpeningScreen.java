121dc9a1d4b9ec1266ad9630fc93a87a7f1ce7b2
Aditya <poudyaladitya@gmail.com>
2019-04-06 22:46:28 -0400

    swiping left

--- app/src/main/java/com/example/autismawareness/OpeningScreen.java
+++ app/src/main/java/com/example/autismawareness/OpeningScreen.java
@@ -1,13 +1,40 @@
 package com.example.autismawareness;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class OpeningScreen extends AppCompatActivity {
+
+    float x1,x2,y1,y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening_screen);
     }
-}
+
+    public boolean onTouchEvent(MotionEvent touchevent) {
+        switch (touchevent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchevent.getX();
+                y1 = touchevent.getY();
+
+            case MotionEvent.ACTION_UP:
+                x2 = touchevent.getX();
+                y2 = touchevent.getY();
+                if (x1 < x2) {
+                    Intent i = new Intent(OpeningScreen.this, SwipeLeft.class);
+                    startActivity(i);
+                }
+                break;
+        }
+        return false;
+    }
+
+
+
+        }
+
+

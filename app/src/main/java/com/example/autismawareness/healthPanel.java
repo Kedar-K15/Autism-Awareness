96093b0ba5ed167a9b0f18616e1da277a126ef44
Aditya <poudyaladitya@gmail.com>
2019-04-07 01:28:17 -0400

    Fixed syntax of names

--- app/src/main/java/com/example/autismawareness/healthPanel.java
+++ app/src/main/java/com/example/autismawareness/healthPanel.java
@@ -1,13 +1,25 @@
 package com.example.autismawareness;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class healthPanel extends AppCompatActivity {
-
+public class HealthPanel extends AppCompatActivity {
+    private Button home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_panel);
+
+        home = (Button) findViewById(R.id.aaware_home);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HealthPanel.this, CategoryList.class));
+            }
+        });
     }
 }

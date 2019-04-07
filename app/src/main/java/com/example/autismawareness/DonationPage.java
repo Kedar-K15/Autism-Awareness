b38b371672bdcb650f986843756d1fe7a1abbaa7
Kedar7833 <Kedar7833@users.noreply.github.com>
2019-04-07 05:25:13 -0400

    added donation link, fixed food page

--- app/src/main/java/com/example/autismawareness/DonationPage.java
+++ app/src/main/java/com/example/autismawareness/DonationPage.java
@@ -1,13 +1,33 @@
 package com.example.autismawareness;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class DonationPage extends AppCompatActivity {
+    private Button home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation_page);
+        home = (Button) findViewById(R.id.aaware_home);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DonationPage.this, CategoryList.class));
+            }
+        });
+
+    }
+    public void goToUrl(View view) {
+        String url = "https://www.gofundme.com/aaware-awareness-for-autism?member=2010884";
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
--- app/src/main/java/com/example/autismawareness/FoodPage.java
+++ app/src/main/java/com/example/autismawareness/FoodPage.java
@@ -1,15 +1,49 @@
 package com.example.autismawareness;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FoodPage extends AppCompatActivity {
+    private Button home;
+    private Button save;
+    private EditText daily_meal_plan;
+    private EditText favorites;
+    private EditText dislikes;
+    private static String str_daily_meal_plan;
+    private static String str_favorites;
+    private static String str_dislikes;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_page);
+        home = (Button) findViewById(R.id.aaware_home);
+        daily_meal_plan = (EditText) findViewById(R.id.Daily_Meal_plan_data);
+        favorites = (EditText) findViewById(R.id.Favorites_data);
+        dislikes = (EditText) findViewById(R.id.Dislike_data2);
+        save = (Button) findViewById(R.id.save_emergency);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FoodPage.this, CategoryList.class));
+            }
+        });
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                str_daily_meal_plan = daily_meal_plan.getText().toString();
+                str_favorites = favorites.getText().toString();
+                str_dislikes = favorites.getText().toString();
+                startActivity(new Intent(FoodPage.this, CategoryList.class));
+            }
+        });
+
+
     }
-
-
 }
